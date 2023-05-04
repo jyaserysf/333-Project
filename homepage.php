e69802c131ee88def494725d521f0ce8749abdac
jyaserysf <[jood.yaser.yusuf@gmail.com]>
2023-05-04 09:19:24 +0300

    homepage + template + css files

---
+++ homepage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="css/homepageStyle.css">
+    <link rel="stylesheet" href="css/generalstyling.css">
+</head>
+<body>
+    <div id="header">
+        
+    </div>
+    <div id="display">
+        <div class="displaysection" id="tagline">
+            <h1> Something about surveys </h1>
+            <h2> Create an account now! </h2>
+            <button action="" > Create </button>
+        </div>
+        <div class="displaysection" id="displayImg">
+            <img src="img\Customer Survey-amico (1).png" width=450px  >
+        </div>
+        
+    </div>
+    <div id="explore">
+
+    </div>
+    <div id="pastsurvey">
+
+    </div>
+    <div id="footer">
+
+    </div>
+
+
+</body>
+</html>
