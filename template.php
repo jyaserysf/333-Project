bed90b0bb5a504e3076cffe10f06f899b5b1ae48
jyaserysf <[jood.yaser.yusuf@gmail.com]>
2023-05-05 16:56:13 +0300

    committing footer and header

--- template.php
+++ template.php
@@ -5,9 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/generalstyling.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/generalstyle.css">
 </head>
 <body>
-    
+    <div id="header"> <?php include 'header.html'?> </div>
+    <div class="cointainer m-4" id="main">
+add content here
+    </div>
+    <div > <?php include 'footer.html'?> </div>
 </body>
 </html>
