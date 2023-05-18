e4be48befc016ef4dbd90e5ffdb4e8e55bb69aaa
sed21v <ahmedsmahmoodedu@gmail.com>
2023-05-18 12:22:02 +0300

    added name column to the database

--- database/surveysphere.sql
+++ database/surveysphere.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2023 at 05:16 PM
+-- Generation Time: May 18, 2023 at 11:21 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -95,12 +95,20 @@ CREATE TABLE `surveys` (
 CREATE TABLE `users` (
   `userID` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
   `email` varchar(100) NOT NULL,
   `role` varchar(50) NOT NULL DEFAULT 'user',
-  `phone` varchar(50) NOT NULL,
-  `image` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `username`, `name`, `email`, `role`, `password`) VALUES
+(1, 'sahmed', 'Ahmed Khalaf', 'abc@gmail.com', 'admin', '$2y$10$dnd54a5uXsZstLRejJH8T.ohSOPzwUbXmvHQS61wVcc2z72c6eZtm'),
+(2, 'jood', 'Jood Yaser', 'jood@gmail.com', 'user', '$2y$10$XAH92dl.sdyeFhc3wQ4B1.huxr8oD3qw.QNceiqTj09tyXYFRvtG.'),
+(3, 'ali1', 'Ali Majeed', 'ali@gmail.com', 'user', '$2y$10$Lf/ZHlhf0as.rjPkm9TNjOFTKmHzvPmKG0AWVsBMqaIqxICcZQf/m');
 
 --
 -- Indexes for dumped tables
@@ -170,7 +178,7 @@ ALTER TABLE `surveys`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
