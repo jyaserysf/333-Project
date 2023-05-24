d0dc97da3cb54d6a7c758110ebeed4563f7ef9ca
Muneera <muneerajaber05@gmail.com>
2023-05-24 09:46:58 +0300

    --

--- Search.php
+++ Search.php
@@ -1,6 +1,32 @@
+
 <?php 
-if(isset($_GET['S'])){
-echo $_GET['S'];
+if(isset($_REQUEST['S'])){?>
+<?php
+    $S = $_REQUEST['S'];
+    $hint = "";
+    try {
+        require('database/connection.php');
+        $SName = $db->prepare("SELECT * from surveys");
+        $SName->execute();
+        $SurveysNames = $SName->fetchAll();
 
+        if($S !== "") {
+            $S = strtolower($S);
+            $len = strlen($S);
+            foreach($SurveysNames as $serv) {
+                if($S == strtolower(substr($serv[4], 0, $len))) {
+                    if($hint === "") {
+                        $hint = $serv[4];
+                    } else {
+                        $hint .= ",{$serv[4]}";
+                    }
+                }
+            }
+        }
 
-}?>
+        echo $hint === "" ? "no suggestion" : $hint;
+    } catch(PDOException $e) {
+        die($e->getMessage());
+    }
+}
+?>
--- javascript/Search.js
+++ javascript/Search.js
@@ -1,33 +1,18 @@
 
-/*function Search(input){
+function Searchbar(input){
 const xhttp=new XMLHttpRequest();
 xhttp.onload=myAJAXFunction;
 xhttp.open("GET","Search.php?S="+input);
 xhttp.send();
 
 }
-
 function myAJAXFunction(){
 document.getElementById("results").innerHTML=this.responseText;
-alert("meow");
-}
-*/
 
-function Search(user){
-    if (user.length == 0){
-        document.getElementById('results').innerText="";
-        return;
-    }
-    const xHttp = new XMLHttpRequest();
-    xHttp.onreadystatechange = function(){
-        if (this.readyState==4 && this.status== 200){
-            document.getElementById('results').innerHTML = this.responseText;
-            
-        }
-    };
-    xHttp.open("GET","Search.php?S="+user)
-    xHttp.send(null);
 }
 
 
+document.getElementById("results").style.display = "none";
 
+
+document.getElementById("results").style.visibility = "hidden".innerHTML=this.responseText;
