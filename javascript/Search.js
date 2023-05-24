ad63d49bdff1bcd803dc80def3a291dac70afae2
Muneera <muneerajaber05@gmail.com>
2023-05-24 11:38:42 +0300

    searchbar - solved the issue ^-^

--- javascript/Search.js
+++ javascript/Search.js
@@ -1,5 +1,9 @@
-
+document.getElementById("results").style.display = "none";
 function Searchbar(input){
+    if (input.length==0){
+        document.getElementById("results").style.display = "none";
+        return;
+    }
 const xhttp=new XMLHttpRequest();
 xhttp.onload=myAJAXFunction;
 xhttp.open("GET","Search.php?S="+input);
@@ -7,9 +11,9 @@ xhttp.send();
 
 }
 function myAJAXFunction(){
+    document.getElementById("results").style.display = "block";
 document.getElementById("results").innerHTML=this.responseText;
 
 }
-
 
 
